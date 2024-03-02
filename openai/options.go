e2d8a37f2e82d2a2f62a5fc847aac3124e4cac28
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-02 06:16:22 +0200

    feat: OpenAI Embedding Models Support
    Refs: #28

--- openai/options.go
+++ openai/options.go
@@ -1,17 +1,46 @@
 package openai
 
+import "fmt"
+
 // Option is a function type that can be used to modify the client.
-type Option func(c *OpenAIClient)
+type Option func(c *OpenAIClient) error
+
+type EmbeddingModel string
+
+const (
+	TextEmbeddingAda002 EmbeddingModel = "text-embedding-ada-002"
+	TextEmbedding3Small EmbeddingModel = "text-embedding-3-small"
+	TextEmbedding3Large EmbeddingModel = "text-embedding-3-large"
+)
 
 // WithOpenAIOrganizationID is an option for setting the OpenAI org id.
 func WithOpenAIOrganizationID(openAiAPIKey string) Option {
-	return func(c *OpenAIClient) {
+	return func(c *OpenAIClient) error {
 		c.SetOrgID(openAiAPIKey)
+		return nil
 	}
 }
 
-func applyClientOptions(c *OpenAIClient, opts ...Option) {
-	for _, opt := range opts {
-		opt(c)
+// WithModel is an option for setting the model to use. Must be one of: text-embedding-ada-002, text-embedding-3-small, text-embedding-3-large
+func WithModel(model EmbeddingModel) Option {
+	return func(c *OpenAIClient) error {
+		if string(model) == "" {
+			return fmt.Errorf("empty model name")
+		}
+		if model != TextEmbeddingAda002 && model != TextEmbedding3Small && model != TextEmbedding3Large {
+			return fmt.Errorf("invalid model name %s. Must be one of: %v", model, []string{string(TextEmbeddingAda002), string(TextEmbedding3Small), string(TextEmbedding3Large)})
+		}
+		c.Model = string(model)
+		return nil
 	}
+}
+
+func applyClientOptions(c *OpenAIClient, opts ...Option) error {
+	for _, opt := range opts {
+		err := opt(c)
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
