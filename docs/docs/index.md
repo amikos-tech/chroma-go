6b1e8cd1aac6013a9c2b61e49f18b1827d72d45f
Trayan Azarov <trayan.azarov@amikos.tech>
2024-03-03 21:53:35 +0200

    docs: Added docs skeleton
    - Updated README.md
    - Added mkdocs workflow
    - Added analytics IDs

---
+++ docs/docs/index.md
@@ -0,0 +1,10 @@
+# Chroma Go Client
+
+An experimental Go client for ChromaDB.
+
+
+## Installation
+
+```bash
+go get github.com/amikos-tech/chroma-go
+```
---
+++ docs/docs/javascripts/gtag.js
@@ -0,0 +1,9 @@
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-NNN722BJKE"/>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments)}
+    gtag('js', new Date());
+
+    gtag('config', 'G-NNN722BJKE');
+</script>
