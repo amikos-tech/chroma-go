819aca07755d1728ed7aed0af2a25ecb0319e329
Trayan Azarov <trayan.azarov@amikos.tech>
2024-04-20 22:18:49 +0300

    feat: Voyage AI embedding support

---
+++ pkg/embeddings/voyage/voyage.go
@@ -0,0 +1,316 @@
+package voyage
+
+import (
+	"bytes"
+	"context"
+	"encoding/base64"
+	"encoding/binary"
+	"encoding/json"
+	"fmt"
+	"io"
+	"math"
+	"net/http"
+
+	"github.com/amikos-tech/chroma-go/types"
+)
+
+// Docs:  https://docs.together.ai/docs/embeddings-rest.  Models - https://docs.together.ai/docs/embeddings-models.
+
+type InputType string
+type EncodingFormat string
+
+const (
+	defaultBaseAPI = "https://api.voyageai.com/v1/embeddings"
+	// https://docs.voyageai.com/docs/embeddings
+	defaultMaxSize                          = 128
+	DefaultTruncation                       = true
+	InputTypeQuery           InputType      = "query"
+	InputTypeDocument        InputType      = "document"
+	defaultModel                            = "voyage-2"
+	EncodingFormatBase64     EncodingFormat = "base64"
+	InputTypeContextVar                     = "inputType"
+	ModelContextVar                         = "model"
+	TruncationContextVar                    = "truncation"
+	EncodingFormatContextVar                = "encodingFormat"
+)
+
+type VoyageAIClient struct {
+	BaseAPI               string
+	APIToken              string
+	DefaultModel          string
+	MaxBatchSize          int
+	DefaultHeaders        map[string]string
+	DefaultTruncation     *bool
+	DefaultEncodingFormat *EncodingFormat
+	Client                *http.Client
+}
+
+func applyDefaults(c *VoyageAIClient) {
+	if c.Client == nil {
+		c.Client = http.DefaultClient
+	}
+	if c.BaseAPI == "" {
+		c.BaseAPI = defaultBaseAPI
+	}
+
+	if c.DefaultTruncation == nil {
+		var defaultTruncation = DefaultTruncation
+		c.DefaultTruncation = &defaultTruncation
+	}
+
+	if c.MaxBatchSize == 0 {
+		c.MaxBatchSize = defaultMaxSize
+	}
+	if c.DefaultModel == "" {
+		c.DefaultModel = defaultModel
+	}
+}
+
+func validate(c *VoyageAIClient) error {
+	if c.APIToken == "" {
+		return fmt.Errorf("API key is required")
+	}
+	if c.MaxBatchSize < 1 {
+		return fmt.Errorf("max batch size must be greater than 0")
+	}
+	if c.MaxBatchSize > defaultMaxSize {
+		return fmt.Errorf("max batch size must be less than %d", defaultMaxSize)
+	}
+	return nil
+}
+
+func NewVoyageAIClient(opts ...Option) (*VoyageAIClient, error) {
+	client := &VoyageAIClient{}
+
+	for _, opt := range opts {
+		err := opt(client)
+		if err != nil {
+			return nil, err
+		}
+	}
+	applyDefaults(client)
+	if err := validate(client); err != nil {
+		return nil, err
+	}
+	return client, nil
+}
+
+type EmbeddingInputs struct {
+	Input  string
+	Inputs []string
+}
+
+func (e *EmbeddingInputs) MarshalJSON() ([]byte, error) {
+	if e.Input != "" {
+		return json.Marshal(e.Input)
+	}
+	if e.Inputs != nil {
+		return json.Marshal(e.Inputs)
+	}
+	return nil, fmt.Errorf("EmbeddingInput has no data")
+}
+
+// from voyageai python client - https://github.com/voyage-ai/voyageai-python/blob/e565fb60b854e80ead526a57ea0e6eb1db9efc33/voyageai/api_resources/embedding.py#L30-L32
+func bytesToFloat32s(b []byte) ([]float32, error) {
+	if len(b)%4 != 0 {
+		return nil, fmt.Errorf("byte slice length must be a multiple of 4")
+	}
+
+	result := make([]float32, len(b)/4)
+	for i := range result {
+		bits := binary.LittleEndian.Uint32(b[i*4:]) // Or binary.BigEndian
+		result[i] = math.Float32frombits(bits)
+	}
+	return result, nil
+}
+
+func (e *EmbeddingTypeResult) UnmarshalJSON(data []byte) error {
+	var str string
+	var floats []float32
+	if err := json.Unmarshal(data, &str); err == nil {
+		decoded, err := base64.StdEncoding.DecodeString(str)
+		if err != nil {
+			return err
+		}
+		e.Floats, err = bytesToFloat32s(decoded)
+		if err != nil {
+			return err
+		}
+		return nil
+	}
+	if err := json.Unmarshal(data, &floats); err == nil {
+		e.Floats = floats
+		return nil
+	}
+	return fmt.Errorf("unexpected data type %v", string(data))
+}
+
+type CreateEmbeddingRequest struct {
+	Model          string           `json:"model"`
+	Input          *EmbeddingInputs `json:"input"`
+	InputType      *InputType       `json:"input_type"`
+	Truncation     *bool            `json:"truncation"`
+	EncodingFormat *EncodingFormat  `json:"encoding_format"`
+}
+
+type EmbeddingTypeResult struct {
+	Floats []float32
+}
+
+type EmbeddingResult struct {
+	Object    string               `json:"object"`
+	Embedding *EmbeddingTypeResult `json:"embedding"`
+	Index     int                  `json:"index"`
+}
+
+type UsageResult struct {
+	TotalTokens int `json:"total_tokens"`
+}
+
+type CreateEmbeddingResponse struct {
+	Object string            `json:"object"`
+	Data   []EmbeddingResult `json:"data"`
+	Model  string            `json:"model"`
+	Usage  *UsageResult      `json:"usage"`
+}
+
+func (c *CreateEmbeddingRequest) JSON() (string, error) {
+	data, err := json.Marshal(c)
+	if err != nil {
+		return "", err
+	}
+	return string(data), nil
+}
+
+func (c *VoyageAIClient) CreateEmbedding(ctx context.Context, req *CreateEmbeddingRequest) (*CreateEmbeddingResponse, error) {
+	if req == nil {
+		return nil, fmt.Errorf("request is nil")
+	}
+	reqJSON, err := req.JSON()
+	if err != nil {
+		return nil, err
+	}
+	httpReq, err := http.NewRequestWithContext(ctx, "POST", c.BaseAPI, bytes.NewBufferString(reqJSON))
+	if err != nil {
+		return nil, err
+	}
+	for k, v := range c.DefaultHeaders {
+		httpReq.Header.Set(k, v)
+	}
+	httpReq.Header.Set("Accept", "application/json")
+	httpReq.Header.Set("Content-Type", "application/json")
+	httpReq.Header.Set("Authorization", "Bearer "+c.APIToken)
+	resp, err := c.Client.Do(httpReq)
+
+	if err != nil {
+		return nil, err
+	}
+	respData, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+	var embeddings CreateEmbeddingResponse
+	if err := json.Unmarshal(respData, &embeddings); err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("unexpected code [%v] while making a request to %v. errors: %v", resp.Status, c.BaseAPI, string(respData))
+	}
+
+	return &embeddings, nil
+}
+
+var _ types.EmbeddingFunction = (*TogetherEmbeddingFunction)(nil)
+
+type TogetherEmbeddingFunction struct {
+	apiClient *VoyageAIClient
+}
+
+func NewVoyageAIEmbeddingFunction(opts ...Option) (*TogetherEmbeddingFunction, error) {
+	client, err := NewVoyageAIClient(opts...)
+	if err != nil {
+		return nil, err
+	}
+
+	return &TogetherEmbeddingFunction{apiClient: client}, nil
+}
+
+// getModel returns the model from the context if it exists, otherwise it returns the default model
+func (e *TogetherEmbeddingFunction) getModel(ctx context.Context) string {
+	model := e.apiClient.DefaultModel
+	if m, ok := ctx.Value(ModelContextVar).(string); ok {
+		model = m
+	}
+	return model
+}
+
+// getTruncation returns the truncation from the context if it exists, otherwise it returns the default truncation
+func (e *TogetherEmbeddingFunction) getTruncation(ctx context.Context) *bool {
+	model := e.apiClient.DefaultTruncation
+	if m, ok := ctx.Value(TruncationContextVar).(*bool); ok {
+		model = m
+	}
+	return model
+}
+
+// getInputType returns the input type from the context if it exists, otherwise it returns the default input type
+func (e *TogetherEmbeddingFunction) getInputType(ctx context.Context, inputType InputType) *InputType {
+	model := &inputType
+	if m, ok := ctx.Value(InputTypeContextVar).(*InputType); ok {
+		model = m
+	}
+	return model
+}
+
+func (e *TogetherEmbeddingFunction) getEncodingFormat(ctx context.Context) *EncodingFormat {
+	model := e.apiClient.DefaultEncodingFormat
+	if m, ok := ctx.Value(EncodingFormatContextVar).(*EncodingFormat); ok {
+		model = m
+	}
+	return model
+}
+
+func (e *TogetherEmbeddingFunction) EmbedDocuments(ctx context.Context, documents []string) ([]*types.Embedding, error) {
+	if len(documents) > e.apiClient.MaxBatchSize {
+		return nil, fmt.Errorf("number of documents exceeds the maximum batch size %v", e.apiClient.MaxBatchSize)
+	}
+	if len(documents) == 0 {
+		return types.NewEmbeddingsFromFloat32(nil), nil
+	}
+
+	req := &CreateEmbeddingRequest{
+		Model:          e.getModel(ctx),
+		Input:          &EmbeddingInputs{Inputs: documents},
+		Truncation:     e.getTruncation(ctx),
+		InputType:      e.getInputType(ctx, InputTypeDocument),
+		EncodingFormat: e.getEncodingFormat(ctx),
+	}
+	response, err := e.apiClient.CreateEmbedding(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	embeddings := make([]*types.Embedding, 0, len(response.Data))
+	for _, result := range response.Data {
+		embeddings = append(embeddings, types.NewEmbeddingFromFloat32(result.Embedding.Floats))
+	}
+	return embeddings, nil
+}
+
+func (e *TogetherEmbeddingFunction) EmbedQuery(ctx context.Context, document string) (*types.Embedding, error) {
+	req := &CreateEmbeddingRequest{
+		Model:          e.getModel(ctx),
+		Input:          &EmbeddingInputs{Input: document},
+		Truncation:     e.getTruncation(ctx),
+		InputType:      e.getInputType(ctx, InputTypeDocument),
+		EncodingFormat: e.getEncodingFormat(ctx),
+	}
+	response, err := e.apiClient.CreateEmbedding(ctx, req)
+	if err != nil {
+		return nil, err
+	}
+	return types.NewEmbeddingFromFloat32(response.Data[0].Embedding.Floats), nil
+}
+
+func (e *TogetherEmbeddingFunction) EmbedRecords(ctx context.Context, records []*types.Record, force bool) error {
+	return types.EmbedRecordsDefaultImpl(e, ctx, records, force)
+}
---
+++ pkg/embeddings/voyage/voyage_test.go
@@ -0,0 +1,214 @@
+//go:build test
+
+package voyage
+
+import (
+	"context"
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"math/rand"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+const charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789 "
+
+func generateRandomString(length int) string {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	b := make([]byte, length)
+	for i := range b {
+		b[i] = charset[seededRand.Intn(len(charset))]
+	}
+	return string(b)
+}
+
+func Test_client(t *testing.T) {
+	apiKey := os.Getenv("VOYAGE_API_KEY")
+	if apiKey == "" {
+		err := godotenv.Load("../../../.env")
+		if err != nil {
+			assert.Failf(t, "Error loading .env file", "%s", err)
+		}
+		apiKey = os.Getenv("VOYAGE_API_KEY")
+	}
+	client, err := NewVoyageAIClient(WithEnvAPIKey())
+	require.NoError(t, err)
+
+	t.Run("Test CreateEmbedding", func(t *testing.T) {
+		req := CreateEmbeddingRequest{
+			Model: "voyage-large-2",
+			Input: &EmbeddingInputs{Input: "Test document"},
+		}
+		resp, rerr := client.CreateEmbedding(context.Background(), &req)
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.NotNil(t, resp.Data)
+		require.Len(t, resp.Data, 1)
+	})
+}
+
+func Test_together_embedding_function(t *testing.T) {
+	apiKey := os.Getenv("VOYAGE_API_KEY")
+	if apiKey == "" {
+		err := godotenv.Load("../../../.env")
+		if err != nil {
+			assert.Failf(t, "Error loading .env file", "%s", err)
+		}
+	}
+
+	t.Run("Test EmbedDocuments with env-based API Key and default model", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		resp, rerr := client.EmbedDocuments(context.Background(), []string{"Test document", "Another test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 2)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with env-based API Key and default model override with context", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		ctx := context.Background()
+		ctx = context.WithValue(ctx, ModelContextVar, "voyage-large-2")
+		resp, rerr := client.EmbedDocuments(ctx, []string{"Test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 1)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1536)
+	})
+
+	t.Run("Test EmbedDocuments for model with env-based API Key", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithDefaultModel("voyage-large-2"))
+		require.NoError(t, err)
+		resp, rerr := client.EmbedDocuments(context.Background(), []string{"Test document", "Another test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 2)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1536)
+	})
+
+	t.Run("Test EmbedDocuments with too large init batch", func(t *testing.T) {
+		_, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithMaxBatchSize(200))
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "max batch size must be less than")
+	})
+
+	t.Run("Test EmbedDocuments with too large batch at inference", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		docs200 := make([]string, 200)
+		for i := 0; i < 200; i++ {
+			docs200[i] = "Test document"
+		}
+		_, err = client.EmbedDocuments(context.Background(), docs200)
+		require.Error(t, err)
+		require.Contains(t, err.Error(), "number of documents exceeds the maximum batch")
+	})
+
+	t.Run("Test EmbedQuery", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		resp, err := client.EmbedQuery(context.Background(), "Test query")
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, *resp.ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with input type document", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		ctx := context.Background()
+		ctx = context.WithValue(ctx, InputTypeContextVar, InputTypeDocument)
+		resp, err := client.EmbedDocuments(ctx, []string{"Test document", "Another test document"})
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with input type query", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		ctx := context.Background()
+		ctx = context.WithValue(ctx, InputTypeContextVar, InputTypeQuery)
+		resp, err := client.EmbedDocuments(ctx, []string{"Test document", "Another test document"})
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with default truncation true", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithTruncation(true), WithDefaultModel("voyage-2"))
+		require.NoError(t, err)
+		resp, err := client.EmbedDocuments(context.Background(), []string{generateRandomString(20000)})
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with default truncation true", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithTruncation(false), WithDefaultModel("voyage-2"))
+		require.NoError(t, err)
+		_, err = client.EmbedDocuments(context.Background(), []string{generateRandomString(20000)})
+		require.NotNil(t, err)
+		require.Contains(t, err.Error(), "your batch has too many tokens")
+	})
+
+	t.Run("Test EmbedDocuments with env-based API Key and WithDefaultHeaders", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithDefaultModel("voyage-large-2"), WithDefaultHeaders(map[string]string{"X-Test-Header": "test"}))
+		require.NoError(t, err)
+		resp, rerr := client.EmbedDocuments(context.Background(), []string{"Test document", "Another test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 2)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1536)
+	})
+
+	t.Run("Test EmbedDocuments with var API Key", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithAPIToken(os.Getenv("VOYAGE_API_KEY")))
+		require.NoError(t, err)
+		resp, rerr := client.EmbedDocuments(context.Background(), []string{"Test document", "Another test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 2)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments with var token and http client", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithAPIToken(os.Getenv("VOYAGE_API_KEY")), WithHTTPClient(http.DefaultClient))
+		require.NoError(t, err)
+		resp, rerr := client.EmbedDocuments(context.Background(), []string{"Test document", "Another test document"})
+
+		require.Nil(t, rerr)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 2)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+	})
+
+	t.Run("Test EmbedDocuments embedding format base64", func(t *testing.T) {
+		client, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey(), WithEncodingFormat(EncodingFormatBase64))
+		require.NoError(t, err)
+		resp, err := client.EmbedDocuments(context.Background(), []string{"Test document"})
+
+		require.Nil(t, err)
+		require.NotNil(t, resp)
+		require.Len(t, resp, 1)
+		require.Len(t, *resp[0].ArrayOfFloat32, 1024)
+
+		clientNoEncoding, err := NewVoyageAIEmbeddingFunction(WithEnvAPIKey())
+		require.NoError(t, err)
+		respNoEncoding, err := clientNoEncoding.EmbedDocuments(context.Background(), []string{"Test document"})
+		require.Nil(t, err)
+		require.NotNil(t, respNoEncoding)
+		require.True(t, resp[0].Compare(respNoEncoding[0]))
+	})
+}
