51d43b4a5c52c9285afb6410d74d9f12ae940317
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-10 08:45:00 +0300

    chore: Wip on result iterator

--- chroma.go
+++ chroma.go
@@ -385,12 +385,13 @@ func (c *Client) Version(ctx context.Context) (string, error) {
 }
 
 type GetResults struct {
-	Ids        []string
-	Documents  []string
-	Metadatas  []map[string]interface{}
-	Embeddings []*types.Embedding
-	collection *Collection
-	PageInfo   *types.PageInfo
+	Ids            []string
+	Documents      []string
+	Metadatas      []map[string]interface{}
+	Embeddings     []*types.Embedding
+	collection     *Collection
+	PageInfo       *types.PageInfo
+	RecordIterator chan *types.Record
 }
 
 func (r *GetResults) NextPage(ctx context.Context) (*GetResults, error) {
@@ -594,6 +595,24 @@ func (c *Collection) GetWithOptions(ctx context.Context, options ...types.Collec
 			QueryOptions: options,
 		}
 	}
+
+	results.RecordIterator = make(chan *types.Record)
+	go func() {
+		count, err := c.Count(ctx)
+		if err != nil {
+			return
+		}
+		start := 0
+		end := count
+		lim := 10
+		if query.Limit != 0 {
+			lim = query.Limit
+		}
+		for i := start; i < end; i += lim {
+			c.GetWithOptions(ctx, types.WithLimit(10), types.WithOffset(i))
+		}
+		close(ch)
+	}()
 	return results, nil
 }
 
