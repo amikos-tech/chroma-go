53be1f39cdd1c2dc3fd847e510e6970e882284e4
Trayan Azarov <trayan.azarov@amikos.tech>
2024-07-26 12:24:49 +0300

    feat: Cohere Reranking Function

---
+++ pkg/rerankings/cohere/cohere.go
@@ -0,0 +1,202 @@
+package cohere
+
+import (
+	"bytes"
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+
+	chromago "github.com/amikos-tech/chroma-go"
+	ccommons "github.com/amikos-tech/chroma-go/pkg/commons/cohere"
+	"github.com/amikos-tech/chroma-go/pkg/rerankings"
+)
+
+type CohereModel = ccommons.CohereModel
+
+const (
+	DefaultRerankingEndpoint               = "rerank"
+	ModelRerankEnglishV30      CohereModel = "rerank-english-v3.0"
+	DefaultModel               CohereModel = ModelRerankEnglishV30
+	ModelRerankMultilingualV30 CohereModel = "rerank-multilingual-v3.0"
+	ModelRerankEnglishV20      CohereModel = "rerank-english-v2.0"
+	ModelRerankMultilingualV20 CohereModel = "rerank-multilingual-v2.0"
+)
+
+type RerankRequest struct {
+	Model           string   `json:"model"`
+	Query           string   `json:"query"`
+	Documents       []any    `json:"documents"`
+	TopN            int      `json:"top_n,omitempty"`
+	RerankFields    []string `json:"rerank_fields,omitempty"`
+	ReturnDocuments bool     `json:"return_documents,omitempty"`
+	MaxChunksPerDoc int      `json:"max_chunks_per_doc,omitempty"`
+}
+
+type RerankResult struct {
+	Document struct {
+		Text string `json:"text"`
+	} `json:"document"`
+	RelevanceScore float32 `json:"relevance_score"`
+	Index          int     `json:"index"`
+}
+
+type RerankResponse struct {
+	ID      string         `json:"id"`
+	Results []RerankResult `json:"results"`
+	Meta    map[string]any `json:"meta"`
+}
+
+type CohereRerankingFunction struct {
+	ccommons.CohereClient
+	TopN            int
+	RerankFields    []string
+	ReturnDocuments bool
+	MaxChunksPerDoc int
+	RerankEndpoint  string
+}
+
+var _ rerankings.RerankingFunction = &CohereRerankingFunction{}
+
+func NewCohereRerankingFunction(opts ...Option) (*CohereRerankingFunction, error) {
+	rf := &CohereRerankingFunction{}
+	ccOpts := make([]ccommons.Option, 0)
+	ccOpts = append(ccOpts, ccommons.WithDefaultModel(DefaultModel))
+	// stagger the options to pass to the cohere client
+	for _, opt := range opts {
+		ccOpts = append(ccOpts, opt(rf))
+	}
+	cohereCommonClient, err := ccommons.NewCohereClient(ccOpts...)
+	if err != nil {
+		return nil, err
+	}
+	rf.CohereClient = *cohereCommonClient
+	rf.RerankEndpoint = cohereCommonClient.GetAPIEndpoint(DefaultRerankingEndpoint)
+	return rf, nil
+}
+
+func (c CohereRerankingFunction) Rerank(ctx context.Context, query string, results []rerankings.Result) (map[string][]rerankings.RankedResult, error) {
+	docs := make([]any, 0)
+	for _, result := range results {
+		d, err := result.ToText()
+		if err != nil {
+			return nil, err
+		}
+		docs = append(docs, d)
+	}
+	req := &RerankRequest{
+		Model:           c.DefaultModel.String(),
+		Query:           query,
+		Documents:       docs,
+		TopN:            c.TopN,
+		RerankFields:    c.RerankFields,
+		ReturnDocuments: c.ReturnDocuments,
+		MaxChunksPerDoc: c.MaxChunksPerDoc,
+	}
+	reqJSON, err := json.Marshal(req)
+	if err != nil {
+		return nil, err
+	}
+	httpReq, err := c.CohereClient.GetRequest(ctx, "POST", c.RerankEndpoint, string(reqJSON))
+	if err != nil {
+		return nil, err
+	}
+	httpReq.Header.Set("Accept", "application/json")
+	httpReq.Header.Set("Content-Type", "application/json")
+	resp, err := c.CohereClient.Client.Do(httpReq)
+	if err != nil {
+		return nil, err
+	}
+	if resp.StatusCode != 200 {
+		var bodyOrError string
+		all, err := io.ReadAll(resp.Body)
+		if err != nil {
+			bodyOrError = err.Error()
+		} else {
+			bodyOrError = string(all)
+		}
+
+		return nil, fmt.Errorf("rerank failed with status code: %d, %v", resp.StatusCode, bodyOrError)
+	}
+	var rerankResp RerankResponse
+	err = json.NewDecoder(resp.Body).Decode(&rerankResp)
+	if err != nil {
+		return nil, err
+	}
+	rankedResults := map[string][]rerankings.RankedResult{c.ID(): make([]rerankings.RankedResult, len(rerankResp.Results))}
+	for i, rr := range rerankResp.Results {
+		originalDoc, err := results[rr.Index].ToText()
+		if err != nil {
+			return nil, err
+		}
+		if rr.Document.Text != "" {
+			originalDoc = rr.Document.Text
+		}
+		rankedResults[c.ID()][i] = rerankings.RankedResult{
+			String: originalDoc,
+			Index:  rr.Index,
+			Rank:   rr.RelevanceScore,
+		}
+	}
+
+	return rankedResults, nil
+}
+
+// ID returns the of the reranking function. We use `cohere-` prefix with the default model
+func (c CohereRerankingFunction) ID() string {
+	return fmt.Sprintf("cohere-%s", c.DefaultModel)
+}
+
+func (c CohereRerankingFunction) RerankResults(ctx context.Context, queryResults *chromago.QueryResults) (*rerankings.RerankedChromaResults, error) {
+	rerankedResults := &rerankings.RerankedChromaResults{
+		QueryResults: *queryResults,
+		Ranks:        map[string][][]float32{c.ID(): make([][]float32, len(queryResults.Ids))},
+	}
+	for i, r := range queryResults.Ids {
+		if len(r) == 0 {
+			return nil, fmt.Errorf("no results to rerank")
+		}
+		docs := make([]any, 0)
+		for _, result := range queryResults.Documents[i] {
+			docs = append(docs, result)
+		}
+		req := &RerankRequest{
+			Model:           c.DefaultModel.String(),
+			Query:           queryResults.QueryTexts[i],
+			Documents:       docs,
+			TopN:            c.TopN,
+			RerankFields:    c.RerankFields,
+			ReturnDocuments: c.ReturnDocuments,
+			MaxChunksPerDoc: c.MaxChunksPerDoc,
+		}
+
+		var bytesBuffer bytes.Buffer
+		err := json.NewEncoder(&bytesBuffer).Encode(req)
+		if err != nil {
+			return nil, err
+		}
+		httpReq, err := c.CohereClient.GetRequest(ctx, "POST", c.RerankEndpoint, bytesBuffer.String())
+		if err != nil {
+			return nil, err
+		}
+		httpReq.Header.Set("Accept", "application/json")
+		httpReq.Header.Set("Content-Type", "application/json")
+		resp, err := c.CohereClient.Client.Do(httpReq)
+		if err != nil {
+			return nil, err
+		}
+		if resp.StatusCode != 200 {
+			return nil, fmt.Errorf("rerank failed with status code: %d, %v", resp.StatusCode, resp.Body)
+		}
+		var rerankResp RerankResponse
+		err = json.NewDecoder(resp.Body).Decode(&rerankResp)
+		if err != nil {
+			return nil, err
+		}
+		rerankedResults.Ranks[c.ID()][i] = make([]float32, len(rerankResp.Results))
+		for _, rr := range rerankResp.Results {
+			rerankedResults.Ranks[c.ID()][i][rr.Index] = rr.RelevanceScore
+		}
+	}
+	return rerankedResults, nil
+}
---
+++ pkg/rerankings/cohere/cohere_test.go
@@ -0,0 +1,335 @@
+package cohere
+
+import (
+	"context"
+	"os"
+	"testing"
+
+	"github.com/joho/godotenv"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+
+	chromago "github.com/amikos-tech/chroma-go"
+	"github.com/amikos-tech/chroma-go/pkg/rerankings"
+)
+
+func TestRerank(t *testing.T) {
+	apiKey := os.Getenv("COHERE_API_KEY")
+	if apiKey == "" {
+		err := godotenv.Load("../../../.env")
+		if err != nil {
+			assert.Failf(t, "Error loading .env file", "%s", err)
+		}
+		apiKey = os.Getenv("COHERE_API_KEY")
+	}
+
+	tests := []struct {
+		name                 string
+		rankingFunction      func() *CohereRerankingFunction
+		query                string
+		results              []any
+		resultsType          string
+		validate             func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult)
+		expectedErrorStrings []string
+		highestScoreIndex    int
+	}{
+		{
+			name: "Test Rerank basic",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithAPIKey(apiKey))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With Env Key",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey())
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With TopN",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey(), WithTopN(2))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+				require.Equal(t, 2, len(results[rf.ID()]))
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With ReturnDocuments",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey(), WithReturnDocuments())
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+				require.Equal(t, 5, len(results[rf.ID()]))
+				for _, rr := range results[rf.ID()] {
+					require.NotEmpty(t, rr.String)
+				}
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With MaxChunksPerDoc",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey(), WithMaxChunksPerDoc(5))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+				require.Equal(t, 5, len(results[rf.ID()]))
+				for _, rr := range results[rf.ID()] {
+					require.NotEmpty(t, rr.String)
+				}
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With Different Model",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey(), WithDefaultModel(ModelRerankMultilingualV30))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+				require.Equal(t, 5, len(results[rf.ID()]))
+				for _, rr := range results[rf.ID()] {
+					require.NotEmpty(t, rr.String)
+				}
+			},
+			highestScoreIndex: 3,
+		},
+		{
+			name: "Test Rerank With RerankFields",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithEnvAPIKey(), WithRerankFields([]string{"content"}))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			query: "What is the capital of the United States?",
+			results: []any{
+				map[string]string{
+					"content": "Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+				},
+				map[string]string{
+					"content": "The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+				},
+				map[string]string{
+					"content": "Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+				},
+				map[string]string{
+					"content": "Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+				},
+				map[string]string{
+					"content": "Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+				},
+			},
+			resultsType: "object",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results map[string][]rerankings.RankedResult) {
+				require.Equal(t, 5, len(results[rf.ID()]))
+				for _, rr := range results[rf.ID()] {
+					require.NotEmpty(t, rr.String)
+				}
+			},
+			highestScoreIndex: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rf := tt.rankingFunction()
+			var resultsToRerank []rerankings.Result
+			if tt.resultsType == "text" {
+				var textResults []string
+				for _, result := range tt.results {
+					textResults = append(textResults, result.(string))
+				}
+				resultsToRerank = rerankings.FromTexts(textResults)
+			} else {
+				resultsToRerank = rerankings.FromObjects(tt.results)
+			}
+			res, err := rf.Rerank(context.Background(), tt.query, resultsToRerank)
+			if len(tt.expectedErrorStrings) > 0 {
+				for _, expectedErrorString := range tt.expectedErrorStrings {
+					require.Error(t, err, "Expected error but got nil")
+					require.Contains(t, err.Error(), expectedErrorString, "Error message does not contain expected string")
+				}
+			} else {
+				require.NoError(t, err, "Failed to rerank")
+				require.NotNil(t, res, "Rerank result is nil")
+				require.Contains(t, res, rf.ID(), "Rerank result does not contain the ID of the reranking function")
+				tt.validate(t, rf, res)
+			}
+			require.NoError(t, err, "Failed to rerank")
+			require.NotNil(t, res, "Rerank result is nil")
+			require.Contains(t, res, rf.ID(), "Rerank result does not contain the ID of the reranking function")
+			tt.validate(t, rf, res)
+			maxIdx, _ := getMaxIDAndRank(res[rf.ID()])
+
+			require.Equal(t, 3, maxIdx, "The most relevant result is not the expected one")
+		})
+	}
+}
+
+func TestRerankChromaResults(t *testing.T) {
+	apiKey := os.Getenv("COHERE_API_KEY")
+	if apiKey == "" {
+		err := godotenv.Load("../../../.env")
+		if err != nil {
+			assert.Failf(t, "Error loading .env file", "%s", err)
+		}
+		apiKey = os.Getenv("COHERE_API_KEY")
+	}
+
+	tests := []struct {
+		name                 string
+		rankingFunction      func() *CohereRerankingFunction
+		results              *chromago.QueryResults
+		resultsType          string
+		validate             func(t *testing.T, rf *CohereRerankingFunction, results *rerankings.RerankedChromaResults)
+		expectedErrorStrings []string
+		highestScoreIndex    int
+	}{
+		{
+			name: "Test Rerank basic",
+			rankingFunction: func() *CohereRerankingFunction {
+				rf, err := NewCohereRerankingFunction(WithAPIKey(apiKey))
+				require.NoError(t, err, "Failed to create CohereRerankingFunction")
+				return rf
+			},
+			results: &chromago.QueryResults{
+				Ids: [][]string{
+					{"1", "2", "3", "4", "5"},
+				},
+				Documents: [][]string{
+					{
+						"Carson City is the capital city of the American state of Nevada. At the 2010 United States Census, Carson City had a population of 55,274.",
+						"The Commonwealth of the Northern Mariana Islands is a group of islands in the Pacific Ocean that are a political division controlled by the United States. Its capital is Saipan.",
+						"Charlotte Amalie is the capital and largest city of the United States Virgin Islands. It has about 20,000 people. The city is on the island of Saint Thomas.",
+						"Washington, D.C. (also known as simply Washington or D.C., and officially as the District of Columbia) is the capital of the United States. It is a federal district. The President of the USA and many major national government offices are in the territory. This makes it the political center of the United States of America.",
+						"Capital punishment (the death penalty) has existed in the United States since before the United States was a country. As of 2017, capital punishment is legal in 30 of the 50 states. The federal government (including the United States military) also uses capital punishment.",
+					},
+				},
+				QueryTexts: []string{"What is the capital of the United States?"},
+			},
+			resultsType: "text",
+			validate: func(t *testing.T, rf *CohereRerankingFunction, results *rerankings.RerankedChromaResults) {
+			},
+			highestScoreIndex: 3,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			rf := tt.rankingFunction()
+			res, err := rf.RerankResults(context.Background(), tt.results)
+			if len(tt.expectedErrorStrings) > 0 {
+				for _, expectedErrorString := range tt.expectedErrorStrings {
+					require.Error(t, err, "Expected error but got nil")
+					require.Contains(t, err.Error(), expectedErrorString, "Error message does not contain expected string")
+				}
+			} else {
+				require.NoError(t, err, "Failed to rerank")
+				require.NotNil(t, res, "Rerank result is nil")
+				require.Contains(t, res.Ranks, rf.ID(), "Rerank result does not contain the ID of the reranking function")
+				tt.validate(t, rf, res)
+			}
+			require.NoError(t, err, "Failed to rerank")
+			require.NotNil(t, res, "Rerank result is nil")
+			require.Contains(t, res.Ranks, rf.ID(), "Rerank result does not contain the ID of the reranking function")
+			tt.validate(t, rf, res)
+			maxIdx := getIDForMaxRank(res.Ranks[rf.ID()][0]) // we have only one query
+			require.Equal(t, 3, maxIdx, "The most relevant result is not the expected one")
+		})
+	}
+}
+
+func getIDForMaxRank(ranks []float32) int {
+	var maxRank = float32(-1)
+	var id = -1
+	for i, rr := range ranks {
+		if maxRank <= rr {
+			id = i
+			maxRank = rr
+		}
+	}
+	return id
+}
+
+func getMaxIDAndRank(results []rerankings.RankedResult) (int, float64) {
+	var maxRank = float64(-1)
+	var id = -1
+	for _, rr := range results {
+		if maxRank <= float64(rr.Rank) {
+			id = rr.Index
+			maxRank = float64(rr.Rank)
+		}
+	}
+
+	return id, maxRank
+}
