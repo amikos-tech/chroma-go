55d19c981e218a4b6f32ca274a668b2300ffa019
Trayan Azarov <trayan.azarov@amikos.tech>
2024-08-16 17:16:57 +0300

    chore: Adding files

---
+++ pkg/embeddings/default_ef/download_utils.go
@@ -0,0 +1,13 @@
+package default_ef
+
+func DownloadOnnxModel() error {
+	return nil
+}
+
+func DownloadTokenizerSharedLibrary() error {
+	return nil
+}
+
+func DownloadOnnxRuntimeSharedLibrary() error {
+	return nil
+}
