9abaa3ba6e639f5e15305cda604594956cdbfb41
Trayan Azarov <trayan.azarov@amikos.tech>
2024-08-23 17:48:28 +0300

    feat: HFEI Rerankers

---
+++ pkg/rerankings/hf/option.go
@@ -0,0 +1,41 @@
+package huggingface
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/amikos-tech/chroma-go/types"
+)
+
+type Option func(c *HFRerankingFunction) error
+
+func WithAPIKey(apiKey string) Option {
+	return func(c *HFRerankingFunction) error {
+		c.apiKey = apiKey
+		return nil
+	}
+}
+
+func WithEnvAPIKey() Option {
+	return func(c *HFRerankingFunction) error {
+		if os.Getenv("HF_API_KEY") == "" {
+			return fmt.Errorf("HF_API_KEY not set")
+		}
+		c.apiKey = os.Getenv("HF_API_KEY")
+		return nil
+	}
+}
+
+func WithModel(model types.RerankingModel) Option {
+	return func(c *HFRerankingFunction) error {
+		c.defaultModel = &model
+		return nil
+	}
+}
+
+func WithRerankingEndpoint(endpoint string) Option {
+	return func(c *HFRerankingFunction) error {
+		c.rerankingEndpoint = endpoint
+		return nil
+	}
+}
