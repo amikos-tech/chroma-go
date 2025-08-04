c6259cf7247d43c462fef7dba7f43f015f85aa80
Trayan Azarov <trayan.azarov@amikos.tech>
2025-08-04 10:49:00 +0300

    chore: Bumping client and collection int tests to use 1.0.15

--- pkg/api/v2/client_http_integration_test.go
+++ pkg/api/v2/client_http_integration_test.go
@@ -25,7 +25,7 @@ import (
 
 func TestClientHTTPIntegration(t *testing.T) {
 	ctx := context.Background()
-	var chromaVersion = "1.0.7"
+	var chromaVersion = "1.0.15"
 	var chromaImage = "ghcr.io/chroma-core/chroma"
 	if os.Getenv("CHROMA_VERSION") != "" {
 		chromaVersion = os.Getenv("CHROMA_VERSION")
