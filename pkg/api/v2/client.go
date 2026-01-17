3e351a6849138a621ae41d09027975beab809907
Trayan Azarov <trayan.azarov@amikos.tech>
2026-01-17 12:47:20 +0200

    fix: add nil response check in debug logging path

--- pkg/api/v2/client.go
+++ pkg/api/v2/client.go
@@ -964,6 +964,10 @@ func (bc *BaseAPIClient) ExecuteRequest(ctx context.Context, method string, path
 		return nil, errors.Wrap(chhttp.ChromaErrorFromHTTPResponse(nil, err), "error sending request")
 	}
 	if bc.logger.IsDebugEnabled() {
+		if resp == nil {
+			bc.logger.Debug("HTTP Response is nil")
+			return nil, errors.New("received nil response from server")
+		}
 		dump, dumpErr := httputil.DumpResponse(resp, true)
 		if dumpErr == nil {
 			bc.logger.Debug("HTTP Response", logger.String("response", _sanitizeResponseDump(string(dump))))
